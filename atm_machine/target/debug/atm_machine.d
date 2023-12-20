5ab3530430209037587fe95b450716d8a07bafef
adarshswaminath <adarshswaminath7@gmail.com>
2023-12-20 10:18:15 +0530

    initial commit Cargo project setup

--- atm_machine/target/debug/atm_machine.d
+++
@@ -1,1 +0,0 @@
-/home/ni8w4lk34/LearningMaterials/rustlang/atm_machine/target/debug/atm_machine: /home/ni8w4lk34/LearningMaterials/rustlang/atm_machine/src/main.rs
--- atm_machine/target/debug/deps/atm_machine-85db34a37db0c03f.d
+++
@@ -1,5 +0,0 @@
-/home/ni8w4lk34/LearningMaterials/rustlang/atm_machine/target/debug/deps/atm_machine-85db34a37db0c03f: src/main.rs
-
-/home/ni8w4lk34/LearningMaterials/rustlang/atm_machine/target/debug/deps/atm_machine-85db34a37db0c03f.d: src/main.rs
-
-src/main.rs:
